07118dba52433523b5e5253c597ad7d23920298b
Silvia Gonzalez <sm.gonzalezm@uniandes.edu.co>
2022-03-13 23:10:19 -0500

    Codigo reproducir video, creo que hay un minimo de tiempo, los de las ejecuciones no los reconoce

--- Prueba.py
+++ Prueba.py
@@ -5,17 +5,19 @@ import numpy as np
 import cv2
 ##
 
-video = cv2.VideoCapture("ambas emg.mp4")
+video = cv2.VideoCapture(r'video.mp4')
 
-##
 while True:
     ret, frame = video.read()
 
     cv2.imshow("video", frame)
 
-    if cv2.waitKey(1) == 27:
+    if cv2.waitKey(27) == 27:
         break
 
 video.release()
 cv2.destroyAllWindows()
+
+
+##
 
